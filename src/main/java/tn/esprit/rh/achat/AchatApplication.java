0a329b034bb28ed1f6d92a94f13fc71a67d47357
unknown <cah44416@gmail.com>
2022-10-28 03:23:23 +0100

    create achat

---
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -0,0 +1,18 @@
+package tn.esprit.rh.achat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@EnableScheduling
+@SpringBootApplication
+public class AchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchatApplication.class, args);
+    }
+
+}
